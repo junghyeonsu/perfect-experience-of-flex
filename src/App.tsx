dbbf9be865fa256ea2ff7fa5829137d779260e03
junghyeonsu <jung660317@naver.com>
2021-01-09 17:23:38 +0900

    eslint & prettier 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const name = [
+  {
+    name: 'hyeonsu',
+  },
+  {
+    name: 'jongong',
+  },
+  {
+    name: 'hi',
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ color: 'red' }}>
+      <p>hi</p>
+      {name.map((person) => (
+        <div key={person.name}>{person.name}</div>
+      ))}
     </div>
   );
 }
-
 export default App;
