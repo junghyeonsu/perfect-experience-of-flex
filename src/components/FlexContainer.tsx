30a7dc19907e36ce1ebaf35f6d51e9452d16ab82
junghyeonsu <jung660317@naver.com>
2021-02-13 23:22:20 +0900

    프로크리에이트로 작업한 이미지 넣어보기

--- src/components/FlexContainer.tsx
+++ src/components/FlexContainer.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
 import FlexItem from 'components/FlexItem';
+// import CSSImage from 'images/css.jpg';
 
 function FlexContainer() {
   return (
     <>
       <p>컨테이너</p>
+      {/* <Image src={CSSImage} alt="이미지1" /> */}
       <Container>
         <FlexItem />
         <FlexItem />
@@ -22,5 +24,10 @@ const Container = styled.div`
   width: 50vw;
   height: 50vh;
 `;
+
+// const Image = styled.img`
+//   width: 100%;
+//   object-fit: cover;
+// `;
 
 export default FlexContainer;
